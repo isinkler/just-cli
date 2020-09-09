12fe94b35e29414c58d9398e73c8a3ebb78bd01e
isinkler <isinkler@me.com>
2020-09-09 14:08:40 +0300

    Add project files.

---
+++ Configuration.cs
@@ -0,0 +1,12 @@
+﻿namespace just_cli
+{
+    public class Configuration
+    {
+        public Commands Commands { get; set; }
+    }
+
+    public class Commands
+    {
+        public string Start { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace just_cli
+{
+    class Program
+    {
+        private const string ConfigFileName = "just.json";
+
+        static void Main(string[] args)
+        {
+            Configuration configuration = GetConfiguration();
+
+            var command = configuration.Commands.Start.Split(' ', 2);
+
+            ExecuteCommand(command);
+        }
+
+        private static Configuration GetConfiguration()
+        {
+            string currentDirectoryPath = Directory.GetCurrentDirectory();
+            string configFilePath = $"{currentDirectoryPath}\\{ConfigFileName}";
+
+            try
+            {
+                string configFileContent = File.ReadAllText(configFilePath);
+
+                Configuration configuration = JsonConvert.DeserializeObject<Configuration>(configFileContent);
+
+                return configuration;
+
+            } catch(FileNotFoundException)
+            {
+                Console.WriteLine("just can't find a just.json file in your current directory.");
+
+                Environment.Exit(0);
+            }
+
+            return default;
+        }
+
+        private static void ExecuteCommand(string[] command)
+        {
+            var process = new Process();
+
+            process.StartInfo = new ProcessStartInfo(command[0], command[1])
+            {
+                UseShellExecute = false
+            };
+
+            process.Start();
+            process.WaitForExit();
+        }
+    }
+}
