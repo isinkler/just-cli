16d6f57484ddab351808803af9ebf838de8de2d4
isinkler <isinkler@me.com>
2020-09-11 22:50:22 +0300

    Use basic executor with the Run command

---
+++ src/Configuration/Config.cs
@@ -0,0 +1,12 @@
+﻿namespace JustCli.Configuration
+{
+    public class Config
+    {
+        public Commands Commands { get; set; }
+    }
+
+    public class Commands
+    {
+        public string Run { get; set; }
+    }
+}
